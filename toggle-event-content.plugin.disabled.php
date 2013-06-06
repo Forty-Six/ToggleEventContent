d3d14a6da6ee12b360b01bb79a466cb7f1fa341a
Forty-Six <forty-six@palladiom.net>
2013-06-06 23:21:59 +0200

    Version initiale de toggle-event-content

---
+++ toggle-event-content.plugin.disabled.php
@@ -0,0 +1,70 @@
+<?php
+/*
+@name ToggleEventContent
+@author forty-six@palladiom.net
+@link https://github.com/Forty-Six
+@licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
+@version 0.1
+@description Ce plugin ajoute un bouton à coté du titre de chaque événement pour en afficher ou non le contenu
+*/
+
+// Ajouter le bouton si le Affichage du contenu de l'article est demandé
+function FS_toggleEventContent_AddButton(&$event) {
+
+        $config = new Configuration();
+        $config->getAll();
+
+	if ($config->get('articleDisplayContent') == '1') {
+		echo '<div onClick="javascript:FS_toggleEventContent(\''.$event->getId().'\');" class="pointer FS_toggleEventContent_Button" alt="Toggle the content" title="Toggle the content" id="FS_toggleEventContent_Button_'.$event->getId().'">Toggle on</div>';
+	}
+}
+
+// Ajouter la balise ouvrante
+function FS_toggleEventContent_Begin(&$event) {
+	echo '<div id="event_'.$event->getId().'" style="display:none">';
+}
+
+// Ajouter la balise fermante
+function FS_toggleEventContent_End(&$event) {
+	echo '</div>';
+}
+
+
+// Ajouter des balises autour des détails et options de l'article affiché entier
+function FS_toggleEventOptions_Begin (&$event) {
+	echo '<div id="eventOptions_'.$event->getId().'" style="display:none">';
+}
+function FS_toggleEventOptions_End (&$event) {
+	echo "</div>";
+}
+
+// Ajouter des balises autour du contenu de l'article
+$FS_config = new Configuration();
+$FS_config->getAll();
+
+if ($FS_config->get('articleDisplayContent') == '1') {
+	
+	switch ($FS_config->get('articleView')) {
+
+		case 'partial':
+		Plugin::addHook("event_pre_description", "FS_toggleEventContent_Begin");
+		Plugin::addHook("event_post_description", "FS_toggleEventContent_End");
+		break;
+		
+		default:
+		case 'complete':
+		Plugin::addHook("event_pre_content", "FS_toggleEventContent_Begin");
+		Plugin::addHook("event_post_content", "FS_toggleEventContent_End");
+		Plugin::addHook("event_pre_bottom_options", "FS_toggleEventOptions_Begin");
+		Plugin::addHook("event_post_bottom_options", "FS_toggleEventOptions_End");
+		break;
+	}
+}
+
+// Insertion du bouton
+Plugin::addHook("event_post_top_options", "FS_toggleEventContent_AddButton");
+
+// Insertion du javascript et du CSS
+Plugin::addJs("/js/main.js");
+plugin::addCss("/css/default.css");
+
