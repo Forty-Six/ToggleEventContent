9371271e161ea770c80d99d6692d57c6be424362
Forty-Six <forty-six@palladiom.net>
2013-06-27 22:27:19 +0200

    Correction de display = '' par display = 'block'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 @author Forty-Six <Forty-Six>
 @link https://github.com/Forty-Six
 @licence CC by nc sa http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
-@version 0.3
+@version 0.4
 @description Ce plugin ajoute un bouton à coté du titre de chaque événement pour en afficher ou non le contenu
 */
 
@@ -18,7 +18,7 @@ function FS_toggleEventContent(element) {
 	eventDetails = eventBloc.children('h3.articleDetails:last-child');
 
 	if (eventContent.css('display') == 'none') {
-		if (eventDetails) eventDetails.css('display', '');
+		if (eventDetails) eventDetails.css('display', 'block');
 		eventButtons.html('Toggle off');
 	} else {
 		eventButtons.html('Toggle on');
